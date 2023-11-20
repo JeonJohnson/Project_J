6a294b15957e9de093b24525505d6b9cf149704f
JeonJohnson <78492604+JeonJohnson@users.noreply.github.com>
2023-11-20 10:51:35 +0900

    Default Setting
    - 공용라이브러리(PublicLib)정리 얼추 완료
    - Singleton 제네릭클래스 수정 중.
    	-> Box생성 관련해서

---
+++ Assets/Scripts/Lib/PublicLib.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+
+public static class Funcs
+{
+	public static bool IsNull<T>(T script) where T : class
+	{
+		return script == null;
+	}
+
+	#region C# default Val Type Casting
+	public static string GetEnumName<T>(int index) where T : struct, IConvertible
+	{//where 조건 struct, IConvertible => Enum으로 제한
+		return Enum.GetName(typeof(T), index);
+	}
+	public static int FlagToEnum(int flag)
+	{
+		if (flag <= 0)
+		{
+			return -1;
+		}
+
+		int temp = flag;
+		int count = 0;
+		while (true)
+		{
+			if (temp == 1)
+			{
+				break;
+			}
+
+			temp = temp >> 1;
+
+			++count;
+		}
+
+		return count;
+	}
+	
+	/// <summary>
+	/// Convert Boolean To Inteager
+	/// </summary>
+	/// <param name="boolean"></param>
+	/// <returns></returns>
+	public static int B2I(bool boolean)
+	{
+		//false => 값 무 (0)
+		//true => 값 유 
+		return Convert.ToInt32(boolean);
+	}
+	/// <summary>
+	/// Convert Inteager To Boolean
+	/// </summary>
+	/// <param name="integer"></param>
+	/// <returns></returns>
+	public static bool I2B(int integer)
+	{
+		return Convert.ToBoolean(integer);
+	}
+	#endregion
+
+	#region Data Structure
+	public static List<T> Shuffle<T>(List<T> list)
+	{
+		for (int i = list.Count - 1; i > 0; i--)
+		{
+			System.Random random = new System.Random(Guid.NewGuid().GetHashCode());
+			int rnd = random.Next(0, i);
+			T temp = list[i];
+			list[i] = list[rnd];
+			list[rnd] = temp;
+		}
+		return list;
+	}
+	#endregion
+
+	#region Find/Search GameObject
+	public static GameObject FindGameObjectInChildrenByName(GameObject Parent, string ObjName)
+	{
+		if (Parent == null)
+		{
+			return null;
+		}
+
+		//그냥 transform.Find 로 찾으면 한 단계 아래 자식들만 확인함.
+		int childrenCount = Parent.transform.childCount;
+
+		GameObject[] findObjs = new GameObject[childrenCount];
+
+		if (Parent.name == ObjName)
+		{
+			return Parent;
+		}
+
+		if (childrenCount == 0)
+		{
+			return null;
+		}
+		else
+		{
+			for (int i = 0; i < childrenCount; ++i)
+			{
+				findObjs[i] = FindGameObjectInChildrenByName(Parent.transform.GetChild(i).gameObject, ObjName);
+
+				if (findObjs[i] != null && findObjs[i].name == ObjName)
+				{
+					return findObjs[i];
+				}
+			}
+
+			return null;
+		}
+	}
+	public static GameObject FindGameObjectInChildrenByTag(GameObject Parent, string ObjTag)
+	{
+		if (Parent == null)
+		{
+			return null;
+		}
+
+		int childrenCount = Parent.transform.childCount;
+
+		GameObject[] findObjs = new GameObject[childrenCount];
+
+		if (Parent.CompareTag(ObjTag))
+		{
+			return Parent;
+		}
+
+		if (childrenCount == 0)
+		{
+			return null;
+		}
+		else
+		{
+			for (int i = 0; i < childrenCount; ++i)
+			{
+				findObjs[i] = FindGameObjectInChildrenByTag(Parent.transform.GetChild(i).gameObject, ObjTag);
+
+				if (findObjs[i] != null && findObjs[i].CompareTag(ObjTag))
+				{
+					return findObjs[i];
+				}
+			}
+			return null;
+		}
+	}
+	public static T FindComponentInNearestParent<T>(Transform curTransform) where T : Component
+	{
+		if (curTransform == null)
+		{
+			return null;
+		}
+
+		T tempComponent = curTransform.GetComponent<T>();
+
+		if (tempComponent == null)
+		{
+			if (curTransform.parent != null)
+			{
+				tempComponent = FindComponentInNearestParent<T>(curTransform.parent);
+			}
+			else
+			{
+				return null;
+			}
+		}
+
+		return tempComponent;
+	}
+
+	/// <summary>
+	/// If dont Exist, Create new GameObject.
+	/// </summary>
+	/// <param name="name">GameObject's Name</param>
+	/// <returns></returns>
+	public static GameObject CheckGameObjectExist(string name)
+	{
+		GameObject temp = GameObject.Find(name);
+
+		if (temp == null)
+		{
+			temp = new GameObject(name);
+		}
+
+		return temp;
+	}
+	/// <summary>
+	/// If dont Exist, Create new GameObject. And Add Component
+	/// <para>
+	/// return GameObject that u find
+	/// </para>
+	/// </summary>
+	/// <typeparam name="T">T is Component that have GameObject to Find </typeparam>
+	/// <param name="objName">GameObject's Name</param>
+	/// <returns></returns>
+	public static GameObject CheckGameObjectExist<T>(string objName) where T : Component
+	{
+		GameObject tempObj = GameObject.Find(objName);
+
+		if (tempObj == null)
+		{
+			tempObj = new GameObject(objName);
+		}
+
+		T tempComponent = tempObj.GetComponent<T>();
+
+		if (tempComponent == null)
+		{
+			tempObj.AddComponent<T>();
+		}
+
+		return tempObj;
+	}
+	/// <summary>
+	/// If dont Exist, Create new GameObject. And Add Component
+	/// <para>
+	/// return Component that u want.
+	/// </para>
+	/// </summary>
+	/// <typeparam name="T">T is Component that have GameObject to Find </typeparam>
+	/// <param name="gameObjectName">GameObject's Name</param>
+	/// <returns></returns>
+	public static T CheckComponentExist<T>(string gameObjectName) where T : Component
+	{
+		GameObject temp = GameObject.Find(gameObjectName);
+
+		if (temp == null)
+		{
+			temp = new GameObject(gameObjectName);
+		}
+
+		T tempComponent = temp.GetComponent<T>();
+
+		if (tempComponent == null)
+		{
+			tempComponent = temp.AddComponent<T>();
+		}
+
+		return tempComponent;
+	}
+	#endregion
+
+	#region Color
+	public static Color SetAlpha(Color color, float alpha)
+	{
+		Color tempColor = color;
+		tempColor.a = alpha;
+		return tempColor;
+	}
+	public static void SetAlpha(ref Color color, float alpha)
+	{
+		Color tempColor = color;
+		tempColor.a = alpha;
+		color = tempColor;
+	}
+	public static void SetGizmoColor(Color color, float alpha = 1f)
+	{
+		Color temp = color;
+		temp.a = alpha;
+		Gizmos.color = temp;
+	}
+	#endregion
+
+	#region Animation
+	public static bool IsAnimationAlmostFinish(Animator animCtrl, string animationName, float ratio = 0.95f)
+	{
+		if (animCtrl.GetCurrentAnimatorStateInfo(0).IsName(animationName))
+		{//여기서 IsName은 애니메이션클립 이름이 아니라 애니메이터 안에 있는 노드이름임
+			if (animCtrl.GetCurrentAnimatorStateInfo(0).normalizedTime >= ratio)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	public static bool IsAnimationCompletelyFinish(Animator animCtrl, string animationName, float ratio = 1.0f)
+	{
+		if (animCtrl.GetCurrentAnimatorStateInfo(0).IsName(animationName))
+		{//여기서 IsName은 애니메이션클립 이름이 아니라 애니메이터 안에 있는 노드이름임
+			if (animCtrl.GetCurrentAnimatorStateInfo(0).normalizedTime >= ratio)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	#endregion
+
+	#region FileIO
+	#endregion
+}
+
+public static class Defines
+{
+	public const float winCX = 1600f;
+	public const float winCY = 900f;
+
+	public const float PI = 3.14159265f;
+
+	public static string managerPrfabFolderPath = "Prefabs/Managers/";
+}
+
+namespace Structs
+{ 
+
+}
---
+++ Assets/Scripts/Lib/Singleton.cs
@@ -0,0 +1,211 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+	//제네뤽 클래스
+	//C++의 템플릿 클래스 비슷한거임
+	private static T instance = null;
+	//public bool isDontDestory;
+	//스스로 Awake에서 Dont Destroy 설정해주셈
+
+	public static T Instance
+	{
+		get
+		{
+			if (instance == null)
+			{
+				instance = InstantiateManager();
+			}
+			return instance;
+		}
+	}
+
+	public static T InstantiateManager(/*bool isDontDestroy*/)
+	{
+		//1. 하이어라키 창에 있는지 확인.
+		//2. 없으면 ManagerPrefabs 폴더안에 같은이름 프리팹 있는지 확인.
+		//3. 없으면 새로 만듬.
+
+		//230530 문제점 발견
+		//Manager를 만약 씬에 올려 놓은 경우, 초기화가 일어난건지 안일어난건지 불확실함.
+		//GameManager의 경우, 씬에 올려놓았을 때, 스크립트 실행순서에 맞춰 가장 먼저 호출된다.
+		//GameManager Awake에서 먼저 다른 매니저들 생성하고 다른 스크립트의 Awake를 다 호출한뒤
+		//다시 RuntimeInitializeOnLoadMethod 어트리뷰트로 생성하는데
+		//instance 자체는 생성하지 않음으로 한무루프에 빠짐.
+		//
+		//근데 Awake에서 instance를 생성하는거 자체가 InstantiateManager 함수를 호출 하는거라서
+		//그것도 한무루프,,,
+
+
+		if (instance == null)
+		{
+			string managerName = typeof(T).Name;
+			GameObject managerObj = null;
+			T tempInstance = FindObjectOfType<T>();
+
+			if (tempInstance)
+			{ //이미 하이어라키창에 오브젝트가 올라가져 있을 경우
+
+				//걍 바로 박스 담아주삼
+				managerObj = tempInstance.gameObject;
+			}
+			else
+			{ //없는 경우 (새로 게임오브젝트 만들어야함)
+			  //1. 프리팹 찾아보기
+				GameObject prefab = Resources.Load(Defines.managerPrfabFolderPath + typeof(T).Name) as GameObject;
+
+				if (prefab)
+				{
+					managerObj = Instantiate(prefab); //여기서 Awake 한번 더 호출함.
+					managerObj.name = managerObj.name.Replace("(Clone)", string.Empty);
+					tempInstance = managerObj.GetComponent<T>();
+				}
+				else
+				{
+					managerObj = new GameObject(managerName);
+					tempInstance = managerObj.AddComponent<T>();//여기서 Awake 한번 더 호출함.
+				}
+			}
+
+			instance = tempInstance;
+
+			GameObject boxObj = FindManagerBoxes(false);
+			managerObj.transform.SetParent(boxObj.transform);
+		}
+
+		return instance;
+	}
+
+
+	public static void InstantiateManagerByPrefabPath(string prefabFolderPath, bool isDontDestroy = true)
+	{
+		//프리팹 있는거 확신 할 때.
+		//앵간하면 InstantiateManager로 퉁치삼
+
+		Debug.LogWarning("U should be use \"InstantiateManager\" Funcs instead of \"InstantiateManagerByPrefabPath\".");
+
+		if (instance == null)
+		{
+			GameObject managerObj = GameObject.Find(typeof(T).Name);
+
+			if (managerObj == null)
+			{
+				GameObject prefab = Resources.Load(prefabFolderPath + typeof(T).Name) as GameObject;
+
+				if (prefab == null)
+				{
+					Debug.LogError(typeof(T).Name + "'s prefab is not exist");
+
+					managerObj = new GameObject(typeof(T).Name);
+					instance = managerObj.AddComponent<T>();
+				}
+				else
+				{
+					managerObj = Instantiate(prefab);
+					instance = managerObj.GetComponent<T>();
+
+					if (instance == null)
+					{
+						Debug.LogError(typeof(T).Name + "'s prefab don't include" + typeof(T).Name + " Script!!");
+					}
+				}
+
+				managerObj.name = managerObj.name.Replace("(Clone)", string.Empty);
+			}
+
+			GameObject boxObj = FindManagerBoxes(isDontDestroy);
+
+
+			if (boxObj != null)
+			{
+				managerObj.transform.SetParent(boxObj.transform);
+			}
+
+		}
+		else { Debug.LogError($"응~~~ {typeof(T).Name}이미 있어~~"); }
+	}
+
+	public static void InstantiateManagerByPrefab(GameObject prefab, bool isDontDestroy = true)
+	{
+		//프리팹 있는거 확신 할 때.
+		//앵간하면 InstantiateManager로 퉁치삼
+
+		Debug.LogWarning("U should be use \"InstantiateManager\" Funcs instead of \"InstantiateManagerByPrefab\".");
+
+		if (prefab.name != typeof(T).Name)
+		{
+			Debug.LogError("매니저 프리팹이랑 실행시키는 곳이랑 다른데?? 확인해확인해확인해확인해");
+		}
+
+		GameObject newObj = null;
+
+		if (instance == null)
+		{
+			newObj = Instantiate(prefab);
+			newObj.name = newObj.name.Replace("(Clone)", string.Empty);
+			instance = newObj.GetComponent<T>();
+
+			GameObject boxObj = FindManagerBoxes(isDontDestroy);
+
+			if (boxObj != null)
+			{
+				newObj.transform.SetParent(boxObj.transform);
+			}
+
+			if (instance == null)
+			{
+				Debug.LogError(typeof(T).Name + "'s prefab don't include" + typeof(T).Name + " Script!!");
+			}
+		}
+		else
+		{
+			Debug.Log($"이미 {typeof(T).Name}있는디?\n확인해확인해확인해");
+		}
+	}
+
+
+	public static GameObject FindManagerBoxes(bool isDontDestroy)
+	{
+		GameObject boxObj = null;
+
+		if (isDontDestroy)
+		{
+			boxObj = Funcs.CheckGameObjectExist("ManagerBox");
+			DontDestroyOnLoad(boxObj);
+		}
+		else
+		{
+			boxObj = Funcs.CheckGameObjectExist("ManagerBox_Destory");
+		}
+
+		return boxObj;
+	}
+
+	
+
+	public virtual void OnEnable()
+	{
+		SceneManager.sceneLoaded += OnSceneChanged;
+
+	}
+
+	public virtual void OnDisable()
+	{
+		SceneManager.sceneLoaded -= OnSceneChanged;
+
+	}
+
+	public virtual void OnSceneChanged(Scene scene, LoadSceneMode mode)
+	{ }
+
+
+	public virtual void OnDestroy()
+	{
+		instance = null;
+	}
+
+}
