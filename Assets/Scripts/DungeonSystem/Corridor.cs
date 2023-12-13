f79e37390f739f4a80240bd985ef6448b65dc3e3
JeonJohnson <78492604+JeonJohnson@users.noreply.github.com>
2023-12-14 04:30:01 +0900

    corridor pivot change & Place Tile

--- Assets/Scripts/DungeonSystem/Corridor.cs
+++ Assets/Scripts/DungeonSystem/Corridor.cs
@@ -1,17 +1,73 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class Corridor : MonoBehaviour
 {
-
 	public Rect rect;
 
-	public Tilemap tileMap;
+	public eDirection dir;
+	
+	public List<Room> linkedRooms;
+	
+	public Tilemap wallTileMap;
+	public Tilemap wayTileMap;
+	public Tile[]	tiles;
+	//타일 까는거는 나중에 외부에서 데이터 가지고 있다가 스테이지에 맞는 타일 깔아 주는 걸로 바꿔야함.
+
+	public Transform planeTr;
 	public SpriteGrid grid;
 
-	public List<Room> linkedRooms;
+	public void SetRect(Rect _rect)
+	{
+		rect = _rect;
+		transform.position = rect.position;
+		
+		planeTr.transform.localScale = new Vector2(rect.width, rect.height);
+		planeTr.transform.localPosition = new Vector2(rect.width * 0.5f, rect.height * 0.5f);
+		grid.UpdateGrid();
+		//실제 타일맵 컴포넌트로 타일 깔아주기
+		SetTiles();
+	}
+
+	public void SetTiles()
+	{
+		switch (dir)
+		{
+			case eDirection.Vertical:
+				{
+					for (int y = 0; y < rect.height; ++y)
+					{
+						for (int x = 0; x < rect.width; ++x)
+						{
+							Tilemap curTilemap = x == 1 ? wayTileMap : wallTileMap;
+
+							curTilemap.SetTile(new Vector3Int(x, y, 0), tiles[x]);
+						}
+					}
+				}
+				break;
+			case eDirection.Horizon:
+				{
+					for (int y = 0; y < rect.height; ++y)
+					{
+						for (int x = 0; x < rect.width; ++x)
+						{
+							Tilemap curTilemap = y == 1 ? wayTileMap : wallTileMap;
+
+							curTilemap.SetTile(new Vector3Int(x, y, 0), tiles[y]);
+						}
+					}
+				}
+				break;
+			default:
+				break;
+		}
+		wallTileMap.gameObject.GetComponent<CompositeCollider2D>().GenerateGeometry();
+
+	}
+	
 
 	private void Awake()
 	{
--- Assets/Scripts/DungeonSystem/Room.cs
+++ Assets/Scripts/DungeonSystem/Room.cs
@@ -14,45 +14,6 @@ public enum RoomType
 	Boss,
 	End
 }
-
-//[System.Serializable]
-//public class CornerPos
-//{
-//	public CornerPos(/*Transform tr*/)
-//	{
-//		//ReCalc(tr);
-//	}
-
-//	public CornerPos(Vector2 lt, Vector3 rt, Vector2 rb, Vector2 lb)
-//	{ 
-//		LT = lt;
-//		RT = rt;
-//		RB = rb;
-//		LB = lb;
-//	}
-	
-//	public Vector2 Size
-//	{ 
-//		get
-//		{return new Vector2(Vector2.Distance(LT, RT), Vector2.Distance(LT, LB)); }
-		
-//	}
-
-//	public void CalcCorner(Transform tr)
-//	{
-//		Vector2 center = tr.position;
-
-//		float halfX = tr.localScale.x * 0.5f;
-//		float halfY = tr.localScale.y * 0.5f;
-
-//		LT = new Vector2(center.x - halfX, center.y + halfY);
-//		RT = new Vector2(center.x + halfX, center.y + halfY);
-//		RB = new Vector2(center.x + halfX, center.y - halfY);
-//		LB = new Vector2(center.x - halfX, center.y - halfY);
-//	}
-	
-//	public Vector2 LT, RT, RB, LB;
-//}
 public class Room : MonoBehaviour
 {
 	public Rect rect;
