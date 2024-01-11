b7c0af39ef7a380702644aedbeed7654df07c557
JeonJohnson <78492604+JeonJohnson@users.noreply.github.com>
2024-01-11 18:09:51 +0900

    Isaac Dungeon Generator Start

---
+++ Assets/Scripts/Editor/DungeonGenerator_Isaac_Editor.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEditor;
+using MoreMountains.Feedbacks;
+
+[CustomEditor(typeof(DungeonGenerator_Isaac))]
+public class DungeonGenerator_Isaac_Editor : Editor
+{
+	//bool isInit = false;
+	public override void OnInspectorGUI()
+	{
+		DungeonGenerator_Drunken generator = (DungeonGenerator_Drunken)target;
+		
+		DrawDefaultInspector();
+
+
+		GUILayout.Label("\nRoom Generator Buttons", EditorStyles.boldLabel);
+		//GUILayout.Label("dont divide upper 12~14 times.\nur Computer hates u...", EditorStyles.miniLabel);
+
+		//1. 초기화 / 구역 나눠주기
+		//기본 바닥 만들어 주기
+
+
+		GUILayout.Label("");
+		if (GUILayout.Button("Test!!!!"))
+		{
+			for (int i = 0; i < generator.createRoomCount; ++i)
+			{
+				generator.CreateRoom();
+			}
+			generator.GotoGameScene();
+
+		}
+
+
+			if (GUILayout.Button("Create One Room"))
+		{
+			generator.CreateRoom();
+		}
+
+
+		//GUILayout.Label("");
+		//if (GUILayout.Button("Add Room"))
+		//{
+		//	generator.Setup();
+		//	generator.CreateRoom();
+		//	//generator.Reset();
+		//}
+
+		GUILayout.Label("");
+		if (GUILayout.Button("reset"))
+		{
+			generator.Reset();
+		}
+
+
+
+
+	}
+
+}
